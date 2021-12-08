621f888db78ef0484c29d7e451287ecf478f3459
Aleks164 <alekseylehamail.ru>
2021-12-08 20:52:40 +0300

    fix coverage

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,22 @@
 module.exports = api => ({
-  plugins: ["@babel/plugin-transform-runtime",],
+  "plugins": [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        "absoluteRuntime": false,
+        "corejs": false,
+        "helpers": true,
+        "regenerator": true,
+        "version": "7.0.0-beta.0"
+      }
+    ]
+  ],
   presets: [
     [
       "@babel/preset-env",
       {
         useBuiltIns: "entry",
-        corejs: "2",
+        corejs: "3",
         targets: api.caller(caller => caller && caller.target === "node")
           ? { node: "current" }
           : { chrome: "58", ie: "11" }
