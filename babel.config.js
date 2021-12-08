accafc9182580494a9f57c7c423326b2f1ac4832
Aleks164 <alekseylehamail.ru>
2021-12-08 21:54:22 +0300

    update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
-module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
-};
+module.exports = api => ({
+  "plugins": [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        "absoluteRuntime": false,
+        "corejs": false,
+        "helpers": true,
+        "regenerator": true,
+        "version": "7.0.0-beta.0"
+      }
+    ]
+  ],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: "3",
+        targets: api.caller(caller => caller && caller.target === "node")
+          ? { node: "current" }
+          : { chrome: "58", ie: "11" }
+      }
+    ]
+  ]
+})
